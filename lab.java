f8f5e6292b7465d60bce21a576cf05c2ae723fb5
KarriPraveen <karripraveen07@gmail.com>
2026-03-26 13:52:32 +0530

    MY Program

---
+++ lab.java
@@ -0,0 +1,9 @@
+class Test{
+    public static void main(String[] args){
+        int a=10;
+        int b=18;
+        int c=17;
+        System.out.println(b/c);
+        System.out.println(a*c);
+    }
+}
